3179711a90e21010e1ea23798ca29a9f2123724e
Daniel Genaro <daniel.silva@irroba.com.br>
2021-04-16 16:48:36 -0300

    feat(Crud): create patient migration

---
+++ database/migrations/2021_04_15_235356_create_patients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone_number')->nullable();
+            $table->date('date_birthday')->nullable();
+            $table->string('cpf')->nullable();
+            $table->enum('gender', ['M', 'F'])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
