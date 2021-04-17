c24f508b3e61972249b6d3246e34728384be4bdd
Daniel Genaro <daniel.silva@irroba.com.br>
2021-04-16 23:06:04 -0300

    fix mask

--- resources/views/utils/scripts.blade.php
+++ resources/views/utils/scripts.blade.php
@@ -1,17 +1,22 @@
 @push('scripts')
     <script>
         $(() => {
-            Inputmask({
-                mask: '999.999.999-99'
-            }).mask(document.getElementById('cpf'));
+            if($('#cpf').length){
+                Inputmask({
+                    mask: '999.999.999-99'
+                }).mask(document.getElementById('cpf'));
+            }
 
-            Inputmask({
-                mask: [
-                    '(99)9999-9999',
-                    '(99)99999-9999',
-                ],
-                keepStatic: true
-            }).mask(document.getElementById('phone_number'));
+
+            if($('#phone_number').length){
+                Inputmask({
+                    mask: [
+                        '(99)9999-9999',
+                        '(99)99999-9999',
+                    ],
+                    keepStatic: true
+                }).mask(document.getElementById('phone_number'));
+            }
         });
     </script>
 @endpush
