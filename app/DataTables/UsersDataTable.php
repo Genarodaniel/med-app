a7f6111b267c8a30ed9e3d06c92ebd6864837171
Daniel Genaro <daniel.silva@irroba.com.br>
2021-04-16 22:17:30 -0300

    feat(Crud User): create controller e datatables

---
+++ app/DataTables/UsersDataTable.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\User;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class UsersDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        $btn = '';
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', function($row){
+                if($row->id === auth()->user()->id){
+
+                    $btn ='<div style = "display:flex;" class="btn-group">';
+                    $btn .= '<a href="'.route("users.edit", $row->id).'" data-toggle="tooltip" data-original-title="Edit" class="edit btn btn-primary btn-sm editItem">Editar</a>';
+                    $btn .= '<button style="margin-left:5px" type="button" onclick="return (confirm(\'Tem certeza que deseja excluir?\') ? document.getElementById(\'form-'.$row->id.'\').submit() : false);" class="btn btn-sm btn-danger">Excluir</button><form action="'.route('users.destroy', $row->id).'" method="POST" id="form-'. $row->id .'">'.csrf_field().'<input name="_method" type="hidden" value="DELETE"></form>';
+                    $btn .= '</div>';
+
+                    return $btn;
+                }
+         })
+         ->editColumn('created_at', function(User $user) {
+            return $user->created_at->format('d/m/Y H:i:s');
+        });
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\User $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(User $model)
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->setTableId('patients-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('Bfrtip')
+                    ->orderBy(1)
+                    ->language('//cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json')
+                    ->buttons(
+                        Button::make('create')->text('Novo usuário'),
+                        Button::make('export')->text('Exportar'),
+                        Button::make('print')->text('Imprimir')
+                    );
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::make('name')->title('Nome'),
+            Column::make('email')->title('E-mail'),
+            Column::make('created_at')->title('Data adição'),
+            Column::computed('action', 'Ações')
+                  ->exportable(false)
+                  ->printable(false)
+                  ->width(60)
+                  ->addClass('text-center')
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'Users_' . date('YmdHis');
+    }
+}
---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Connections\UserConnection;
+use App\DataTables\UsersDataTable;
+use Illuminate\Validation\Rule;
+
+class UsersController extends Controller
+{
+    protected $userConnection;
+
+    public function __construct(UserConnection $userConnection)
+    {
+        $this->userConnection = $userConnection;
+    }
+    public function index (UsersDataTable $dataTable)
+    {
+        return $dataTable->render('users.index');
+    }
+
+    /**
+     * Return create user form
+     */
+    public function create ()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required','unique:users', 'string', 'email', 'max:255'],
+            'password' => ['required' , 'min:8','confirmed']
+        ]);
+
+        $this->userConnection->store($request->all());
+
+        return redirect()->route('users.index')->with('success','Usuário adicionado!');
+
+    }
+
+    public function destroy($id)
+    {
+        $user = $this->userConnection->destroy($id);
+
+        return redirect()->route('users.index')->with('success', 'Usuário deletado!');
+
+    }
+
+    public function edit($id)
+    {
+        $user = $this->userConnection->show($id);
+
+        if (!$user) {
+            return redirect()->route('users.index')->with('error','Usuário não encontrado');
+        }
+
+        return view('users.edit')->with(compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $validator = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', Rule::unique('users')->ignore($id), 'string', 'email', 'max:255'],
+            'password' => ['required' , 'min:8','confirmed']
+        ]);
+
+        $user = $this->userConnection->update($id,$request->all());
+
+        return redirect()->route('users.index')->with('success','Usuário editado!');
+
+    }
+
+}
